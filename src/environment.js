866ee1735194c29c9f6f9651eb9d51de332e5ca5
Vicky Kumar Kushwaha <vickykumarkusgwaha@gmail.com>
2025-07-18 21:03:47 +0530

    commit change

--- src/environment.js
+++ src/environment.js
@@ -1,8 +1,8 @@
 let IS_PROD = true;
 const server = IS_PROD ?
     "https://apnacollegebackend.onrender.com" :
-    // "https://8de2d6a99997.ngrok-free.app"
-    "http://192.168.130.28:8000"
+    "https://3c83a01102d9.ngrok-free.app"
+    // "http://192.168.130.28:8000"
 
 
 export default server;
